f2b173b4e4e5bba0d186bbcd57721bfc4ff8b301
arunreddy826906 <arunreddy826@gmail.com>
2024-03-26 19:21:39 +0530

    new changes

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "jenkins-terraform" {
+  ami = "ami-05295b6e6c790593e"
+  key_name = "ArunDevOpsKeyPair"
+  tags = {
+    Name="jenkins"
+  }
+  instance_type = "t2,micro"
+}
